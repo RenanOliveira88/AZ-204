e4ced57de28a488288bf26d8bd8a8cfe62fed7e1
renan <renan@gmail.com.br>
2025-03-11 12:57:58 -0300

    AZ-204 - Project initialization

---
+++ DEV/src/main/java/org/examples/az204/DEV.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.examples.az204;
+
+/**
+ *
+ * @author crena
+ */
+public class DEV {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
